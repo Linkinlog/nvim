9255ec5aeb6f9aba059fb54d300cfa31dbd04d96
Linkinlog <dahlton@dahlton.org>
2023-04-17 12:58:49 -0400

    Feat: remove pint, add prettierd

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -3,9 +3,9 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.autopep8,
         null_ls.builtins.formatting.rustfmt,
-        null_ls.builtins.formatting.pint,
         null_ls.builtins.formatting.shellharden,
         null_ls.builtins.formatting.shfmt,
         null_ls.builtins.formatting.beautysh,
+        null_ls.builtins.formatting.prettierd,
     }
 })
