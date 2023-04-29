27dfaf7da4ee9aac8dcff192fc165e3ffd73a5d7
Linkinlog <dahlton@dahlton.org>
2023-04-28 23:03:46 -0400

    fix(lsp): Stop autocompleting on <CR>
    And removes dead code

--- after/plugin/lspzero.lua
+++ after/plugin/lspzero.lua
@@ -17,7 +17,7 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ["<C-Space>"] = cmp.mapping.complete(),
 })
 
@@ -40,13 +40,8 @@ lsp.set_preferences({
     }
 })
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(function(_, bufnr)
     local opts = { buffer = bufnr, remap = false }
-
-    if client.name == "eslint" then
-        vim.cmd [[ LspStop eslint ]]
-        return
-    end
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
