17d6aa32f9a01562b3e1d0633770099665f9f234
Linkinlog <dahlton@dahlton.org>
2023-04-28 23:04:43 -0400

    feat(keybindings): Adds keybinding to view help tags

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,3 +6,4 @@ vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
