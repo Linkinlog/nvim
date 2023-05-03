53dae7fb983df33d2a5c42a7c3ae1217a3ed2b3c
Linkinlog <dahlton@dahlton.org>
2023-05-03 08:47:19 -0400

    feat(discordPresense): Did a funny

--- after/plugin/discordPresense.lua
+++ after/plugin/discordPresense.lua
@@ -1,7 +1,8 @@
 require("presence").setup({
-    neovim_image_text   = "VSCode", -- Text displayed when hovered over the Neovim image
+    neovim_image_text   = "We Buy Any Car!", -- Text displayed when hovered over the Neovim image
     -- Rich Presence text options
     editing_text        = "Addin bugs to %s",               -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+    client_id           = "1103104808635879435",
     file_explorer_text  = "Perusin %s",              -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
     git_commit_text     = "Committin changes straight to prod",       -- Format string rendered when committing changes in git (either string or function(filename: string): string)
     plugin_manager_text = "Managin plugins",         -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
