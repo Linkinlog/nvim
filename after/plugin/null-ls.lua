0c2e6a819c00d173cf3e55a30ba1879441df3227
Linkinlog <dahlton@dahlton.org>
2023-09-13 09:51:23 -0400

    fix: remove revive from null-ls

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -7,6 +7,5 @@ null_ls.setup({
         null_ls.builtins.formatting.shfmt,
         null_ls.builtins.formatting.beautysh,
         null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.diagnostics.revive,
     }
 })
