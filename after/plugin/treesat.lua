5c5d5e82627af214fb2decdc41899001c545fb35
Linkinlog <dahlton@dahlton.org>
2024-01-04 08:41:29 -0500

    fix(treesitter): remove deprecated parser

--- after/plugin/treesat.lua
+++ after/plugin/treesat.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all"
-	ensure_installed = { "vimdoc", "help", "go", "javascript", "php", "c", "lua", "rust" },
+	ensure_installed = { "vimdoc", "go", "javascript", "php", "c", "lua", "rust" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
