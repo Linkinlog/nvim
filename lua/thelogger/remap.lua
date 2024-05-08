f830fb301a978efae8ee86d12d52ccbfd9422a04
Linkinlog <dahlton@dahlton.org>
2024-05-08 09:24:06 -0400

    fix: keymap & go bloat

--- lua/thelogger/remap.lua
+++ lua/thelogger/remap.lua
@@ -55,7 +55,7 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- For going inside of {} on a newline
-vim.keymap.set("n", "<C-m>", "i<CR><Esc>O")
+vim.keymap.set("n", "<C-n>", "i<CR><Esc>O")
 
 
 -- Go to split window when open
