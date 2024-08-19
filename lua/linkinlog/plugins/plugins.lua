b39fd53fbcf8c50267695697059568a9bb26a005
Linkinlog <dahlton@dahlton.org>
2024-08-18 21:33:34 -0400

    fix: remove vim-sleuth

--- lua/linkinlog/plugins/plugins.lua
+++ lua/linkinlog/plugins/plugins.lua
@@ -2,7 +2,6 @@ return {
 	{ "earthly/earthly.vim", ft = "Earthfile" },
 	{ "github/copilot.vim", event = { "BufAdd", "BufReadPre" } },
 	{ "numToStr/Comment.nvim", event = { "BufAdd", "BufReadPre" }, opts = {} },
-	{ "tpope/vim-sleuth", event = { "BufAdd", "BufReadPre" } },
 	{
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPre", "BufAdd" },
